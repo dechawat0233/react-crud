d85204a316923399d0b2df7588372f7974826793
dechawat0233 <69790549+dechawat0233@users.noreply.github.com>
2022-12-25 12:34:37 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+// import React from "react";
+import * as React from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+// import { BrowserRouter ,Router, Route, Link, Routes } from 'react-router-dom';
+import Navbar from "./Navbar"
+import Users from "./Users"
+import UserCreate from "./UserCreate"
+import UserUpdate from './UserUpdate'
+
+
+export default function App() {
+  return (
+
+    // <Router>
+
+    //   <Navbar />
+    //   <Users />
+    //   <UserCreate />
+    // </Router>
+
+    // <Router>
+    //   <Navbar />
+    //   <div>
+    //     <Routes>
+    //       <Route exact path='/'>
+    //         <Users />
+    //       </Route>
+    //       <Route path='/UserCreate'>
+    //         <UserCreate />
+    //       </Route>
+    //     </Routes>
+    //   </div>
+    // </Router>
+
+    <Router>
+      <Navbar />
+        <Routes>
+          
+          <Route exact path='/' element={ <Users /> } />
+          <Route path='/create' element={ <UserCreate /> } />
+          <Route path='/update/:id' element={ <UserUpdate /> } />
+          {/* <Route exact path='/' component={Users} />
+          <Route path='/create' component={UserCreate} /> */}
+        </Routes>
+     </Router>
+
+  );
+}
+
+
+// function Users() {
+//   return <h2>Users</h2>;
+// }
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+
+// function Users() {
+//   return <h2>Users</h2>;
+// }
---
+++ src/Navbar.js
@@ -0,0 +1,98 @@
+import * as React from 'react';
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  navlink:{
+    color: 'white',
+    textDecoration: 'none'
+  }
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Link className={classes.navlink} to ="/">
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              CRUD APP
+            </Typography>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+// import React from "react";
+// import { makeStyles } from '@material-ui/core/styles';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import { Link } from "react-router-dom";
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     flexGrow: 1,
+//   },
+//   navlink:{
+//     color: 'white',
+//     textDecoration: 'none'
+//   }
+// }));
+
+// export default function App() {
+//   const classes = useStyles();
+
+//   return (
+//     <div className={classes.root}>
+//       <AppBar position="static">
+//         <Toolbar>
+//           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+//             <MenuIcon />
+//           </IconButton>
+//           <Link className={classes.navlink} to="/">
+//             <Typography variant="h6" className={classes.title}>
+//               CRUD
+//             </Typography>
+//           </Link>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
